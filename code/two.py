2bb2788e513e0e91edc8984138075fb4b1ca4538
sajad <sajadbanazadeh@gmail.com>
2024-10-31 08:21:41 +0330

    create a example , that have response by method backend in app celery ,  line 7

---
+++ code/two.py
@@ -0,0 +1,17 @@
+from time import sleep
+from celery import Celery
+
+# Configure the Celery app to use RabbitMQ
+app = Celery(
+    main='name_app',
+    backend='rpc://',
+    broker='pyamqp://bob:bob@localhost//'
+)
+
+# Set the broker_connection_retry_on_startup to True
+app.conf.broker_connection_retry_on_startup = True
+
+@app.task
+def add(x, y):
+    sleep(5)
+    return (x+y)
