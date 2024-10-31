2b87ff1b05fa9d7b4dbec5ed824b0c2b9ffb2dcb
sajad <sajadbanazadeh@gmail.com>
2024-10-31 08:24:49 +0330

    create a run file python to send request to celey and get response, line 10 , get response witho timeout. ok , but have time out exception

--- code/run_two_resive_result.py
+++ code/run_two_resive_result.py
@@ -6,3 +6,5 @@ result = add.delay(5,8)
 # get result without time out , unlimited loop, not good !
 print(result.get())
 
+# get result with time out , have timeout error exception, ok.
+print(result.get(timeout=3))
