ad2ffc0d7012f594c2a815621f44aba8fe9f2fa0
sajad <sajadbanazadeh@gmail.com>
2024-10-31 08:27:42 +0330

    create an if ,to check have result or not

--- code/run_two_resive_result.py
+++ code/run_two_resive_result.py
@@ -4,7 +4,10 @@ from two import add
 result = add.delay(5,8)
 
 # get result without time out , unlimited loop, not good !
-print(result.get())
+# print(result.get())
 
-# get result with time out , have timeout error exception, ok.
-print(result.get(timeout=3))
+# get result with time out ,but have timeout error exception, ok.
+# print(result.get(timeout=3))
+
+if result.ready():
+    print(result.get(timeout=3))
